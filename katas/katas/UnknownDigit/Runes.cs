900de51e6518f380fae1dda249fa84565cf7d1cb
agrocks <gabuzaav@gmail.com>
2016-11-10 00:47:47 +0100

    Unknown Digit problem

---
+++ katas/katas/UnknownDigit/Runes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace katas.UnknownDigit
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/find-the-unknown-digit
+    /// </summary>
+    public class Runes
+    {
+        public static int solveExpression(string expression)
+        {
+            int missingDigit = -1;
+            var dt = new DataTable();
+
+            //Write code to determine the missing digit or unknown rune
+            //Expression will always be in the form
+            //(number)[opperator](number)=(number)
+            //Unknown digit will not be the same as any other digits used in expression
+            // number can't start with 0 (e.g 00,01,02,..)
+
+            var regexp = new Regex(@"\d");
+            var digits = regexp.Matches(expression).OfType<Match>().SelectMany(x => x.Value.Select(y => int.Parse(y.ToString()))).Distinct();
+            var possibleDigits = Enumerable.Range(0, 10).Select(x => x).Except(digits);
+            if (expression.Contains("??")) possibleDigits = possibleDigits.Except(new List<int>() { 0 }); // 00 can't be an option
+            foreach (var possibleDigit in possibleDigits)
+            {
+                var result = dt.Compute(expression.Replace('?', possibleDigit.ToString()[0]), null);
+                if ((bool)result) return possibleDigit;
+            }
+
+            return missingDigit;
+        }
+    }
+}
---
+++ katas/katas/UnknownDigit/RunesTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace katas.UnknownDigit
+{
+    [TestFixture]
+    public class RunesTest
+    {
+        [Test]
+        public void testSample()
+        {
+            Assert.AreEqual(2, Runes.solveExpression("1+1=?"), "Answer for expression '1+1=?' ");
+            Assert.AreEqual(6, Runes.solveExpression("123*45?=5?088"), "Answer for expression '123*45?=5?088' ");
+            Assert.AreEqual(0, Runes.solveExpression("-5?*-1=5?"), "Answer for expression '-5?*-1=5?' ");
+            Assert.AreEqual(-1, Runes.solveExpression("19--45=5?"), "Answer for expression '19--45=5?' ");
+            Assert.AreEqual(5, Runes.solveExpression("??*??=302?"), "Answer for expression '??*??=302?' ");
+            Assert.AreEqual(2, Runes.solveExpression("?*11=??"), "Answer for expression '?*11=??' ");
+        }
+    }
+}
