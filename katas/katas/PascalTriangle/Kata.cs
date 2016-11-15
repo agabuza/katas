3acef2ffdb79a328cb74dbf015b4a6863628eca2
agrocks <gabuzaav@gmail.com>
2016-11-15 10:54:31 +0100

    Pascal Triangle kata

---
+++ katas/katas/PascalTriangle/Kata.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace katas.PascalTriangle
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5226eb40316b56c8d500030f
+    /// </summary>
+    public static class Kata
+    {
+        public static List<int> PascalsTriangle(int n)
+        {
+            var result = new List<int> {1};
+
+            // C(row, index) = C(row, index - 1) * (row + 1 - index) / (index)
+            for (int r = 1; r < n; r++)
+            {
+                var row = new List<int> { 1 };
+                for (int i = 1; i <= r; i++)
+                    row.Add(row[i - 1] * (r + 1 - i) / i);
+                result.AddRange(row);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ katas/katas/PascalTriangle/PascalsTriangleTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace katas.PascalTriangle
+{
+    [TestFixture]
+    public class PascalsTriangleTests
+    {
+        [Test]
+        public static void Level1()
+        {
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1 }, Kata.PascalsTriangle(4));
+            CollectionAssert.AreEqual(new List<int> { 1 }, Kata.PascalsTriangle(1));
+        }
+    }
+}
