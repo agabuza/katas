107ffd28e641464dd4d737647a4005381ab5b036
agrocks <gabuzaav@gmail.com>
2016-11-17 00:15:43 +0100

    Next big number kata

---
+++ katas/katas/NextBiggerNumber/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace katas.NextBiggerNumber
+{
+public class Kata
+{
+    public static long NextBiggerNumber(long n)
+    {
+        var digits = n.ToString().ToArray();
+        for (var i = digits.Length - 2; i >= 0; i--)
+        {
+            if (digits[i] < digits[i + 1])
+            {
+                for (int j = i; j < digits.Length; j++)
+                    if (digits[i] < digits[j])
+                    {
+                        var minValue = digits[i];
+                        digits[i] = digits[j];
+                        digits[j] = minValue;
+                        var firstPart = digits.Take(i).ToArray();
+                        var secondPart = digits.Skip(i).Take(n.ToString().Length - i).ToArray();
+
+                        return long.Parse(new string(firstPart) + new string(secondPart));
+                    }
+            }
+        }
+
+        return -1;
+    }
+}
+}
---
+++ katas/katas/NextBiggerNumber/NextBiggerNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace katas.NextBiggerNumber
+{
+    [TestFixture]
+    public class NextBiggerNumberTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Small Number");
+            Assert.AreEqual(21, Kata.NextBiggerNumber(12));
+            Assert.AreEqual(531, Kata.NextBiggerNumber(513));
+            Assert.AreEqual(2071, Kata.NextBiggerNumber(2017));
+            Assert.AreEqual(441, Kata.NextBiggerNumber(414));
+            Assert.AreEqual(414, Kata.NextBiggerNumber(144));
+        }
+    }
+}
