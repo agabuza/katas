9368fc7c765d0b59dbf5b9b0584833e6597d45b4
agrocks <gabuzaav@gmail.com>
2016-11-17 11:44:16 +0100

    Fixes to Next bigger number kata

--- katas/katas/NextBiggerNumber/Kata.cs
+++ katas/katas/NextBiggerNumber/Kata.cs
@@ -2,30 +2,40 @@
 
 namespace katas.NextBiggerNumber
 {
-public class Kata
-{
-    public static long NextBiggerNumber(long n)
+
+    /// <summary>
+    /// https://www.codewars.com/kata/next-bigger-number-with-the-same-digits/train/csharp
+    /// </summary>
+    public class Kata
     {
-        var digits = n.ToString().ToArray();
-        for (var i = digits.Length - 2; i >= 0; i--)
+        public static long NextBiggerNumber(long n)
         {
-            if (digits[i] < digits[i + 1])
+            var digits = n.ToString().ToArray();
+            for (var i = digits.Length - 2; i >= 0; i--)
             {
-                for (int j = i; j < digits.Length; j++)
-                    if (digits[i] < digits[j])
-                    {
-                        var minValue = digits[i];
-                        digits[i] = digits[j];
-                        digits[j] = minValue;
-                        var firstPart = digits.Take(i).ToArray();
-                        var secondPart = digits.Skip(i).Take(n.ToString().Length - i).ToArray();
+                if (digits[i] < digits[i + 1])
+                {
+                    var position = 0;
+                    var minD = ':'; // ['0'-'9'] < ':'
+                    for (int j = i + 1; j < digits.Length; j++)
+                        if (digits[i] < digits[j] && digits[j] < minD)
+                        {
+                            minD = digits[j];
+                            position = j;
+                        }
 
-                        return long.Parse(new string(firstPart) + new string(secondPart));
-                    }
+                    var minValue = digits[i];
+                    digits[i] = digits[position];
+                    digits[position] = minValue;
+
+                    var firstPart = digits.Take(i + 1).ToArray();
+                    var secondPart = digits.Skip(i + 1).Take(n.ToString().Length - i - 1).OrderBy(x => x).ToArray();
+
+                    return long.Parse(new string(firstPart) + new string(secondPart));
+                }
             }
-        }
 
-        return -1;
+            return -1;
+        }
     }
-}
 }
--- katas/katas/NextBiggerNumber/NextBiggerNumberTests.cs
+++ katas/katas/NextBiggerNumber/NextBiggerNumberTests.cs
@@ -10,11 +10,16 @@ namespace katas.NextBiggerNumber
         public void Test1()
         {
             Console.WriteLine("****** Small Number");
+            Assert.AreEqual(1234567908, Kata.NextBiggerNumber(1234567890));
+            Assert.AreEqual(169191738, Kata.NextBiggerNumber(169191387));
+            Assert.AreEqual(59884848483559, Kata.NextBiggerNumber(59884848459853));
+
             Assert.AreEqual(21, Kata.NextBiggerNumber(12));
             Assert.AreEqual(531, Kata.NextBiggerNumber(513));
             Assert.AreEqual(2071, Kata.NextBiggerNumber(2017));
             Assert.AreEqual(441, Kata.NextBiggerNumber(414));
             Assert.AreEqual(414, Kata.NextBiggerNumber(144));
+
         }
     }
 }
