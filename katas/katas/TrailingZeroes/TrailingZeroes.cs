2dc6a0b58c9fdbf6c62630b6b182acfbb010d2a7
agrocks <gabuzaav@gmail.com>
2020-01-26 00:47:30 +0100

    added kata desctiption

--- katas/katas/TrailingZeroes/TrailingZeroes.cs
+++ katas/katas/TrailingZeroes/TrailingZeroes.cs
@@ -1,4 +1,8 @@
-﻿namespace katas.TrailingZeroes
+﻿//Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+//N! = 1 * 2 * 3 * ... * N
+//Be careful 1000! has 2568 digits...
+
+namespace katas.TrailingZeroes
 {
     public static class TrailingZeroes
     {
