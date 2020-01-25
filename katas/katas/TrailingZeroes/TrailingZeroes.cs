7b70b9748ba99839d1529499a5d03d447e773ba3
agrocks <gabuzaav@gmail.com>
2020-01-26 00:40:14 +0100

    trailing zeroes kata

---
+++ katas/katas/TrailingZeroes/TrailingZeroes.cs
@@ -0,0 +1,18 @@
+﻿namespace katas.TrailingZeroes
+{
+    public static class TrailingZeroes
+    {
+        public static int GetTrailingFactorialZeroes(int n)
+        {
+            var trailingZeroes = 0;
+            var baseDiv = 5;
+            while (n >= baseDiv)
+            {
+                trailingZeroes += n / baseDiv;
+                baseDiv *= 5;
+            }
+
+            return trailingZeroes;
+        }
+    }
+}
