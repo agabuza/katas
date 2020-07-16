b31f17acd9c5c2554446e54ef101777e312c9477
Anatolii Gabuza <anatolii.gabuza>
2020-07-16 13:08:39 +0200

    RangeExtraction

---
+++ katas/katas/EulersODE/EulerOde.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// https://www.codewars.com/kata/56347fcfd086de8f11000014
+/// </summary>
+namespace katas.EulersODE
+{
+    public class EulerOde
+    {
+        public static double ExEuler(int nb)
+        {
+            // your code
+            return 0;
+        }
+    }
+}
---
+++ katas/katas/EulersODE/EulerOdeTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace katas.EulersODE
+{
+    [TestFixture]
+    public static class EulerOdeTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Testing ExEuler");
+            Assert.AreEqual(0.5, EulerOde.ExEuler(1));
+            Assert.AreEqual(0.026314, EulerOde.ExEuler(10));
+            Assert.AreEqual(0.015193, EulerOde.ExEuler(17));
+            Assert.AreEqual(0.005073, EulerOde.ExEuler(50));
+            Assert.AreEqual(0.002524, EulerOde.ExEuler(100));
+        }
+    }
+}
