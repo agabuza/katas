ff2c28579f9a7acb8cbba937e5e1d9d77134af78
Anatolii Gabuza <anatolii.gabuza>
2020-07-23 15:21:12 +0200

    morse code final

--- katas/katas/MorseCode/MorseCodeDecoder.cs
+++ katas/katas/MorseCode/MorseCodeDecoder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 /// <summary>
 /// https://www.codewars.com/kata/54b72c16cd7f5154e9000457/
@@ -61,50 +62,68 @@ namespace katas.MorseCode
         public static string DecodeBits(string bits)
         {
             bits = bits.TrimStart('0').TrimEnd('0');
-            var timeUnit = int.MaxValue;
-            for (int i = 0; i < bits.Length; i++)
-            {
-                if (bits[i] == '0')
-                {
-                    var start = i;
-                    while (i < bits.Length && bits[i] == '0') i++;
-                    var size = i - start;
 
-                    if (size < timeUnit)
-                        timeUnit = size;
+            if (!bits.Contains("0")) return ".";
+
+            var timeUnit = int.MaxValue;
+            var index = 0;
+            while (index < bits.Length)
+            {
+                var length = 0;
+                var start = index;
+                while (index < bits.Length && bits[start] == bits[index])
+                {
+                    index++;
+                    length++;
                 }
+
+                if (length < timeUnit)
+                    timeUnit = length;
             }
 
-            var index = 0;
-            string morse = string.Empty;
+            index = 0;
+            var morse = new StringBuilder();
             while (index < bits.Length)
             {
                 char currentBit = bits[index];
-                var length = 1;
-
-                while ((index < bits.Length - 1) && bits[++index] == currentBit) length++;
+                var length = 0;
+                while (index < bits.Length && bits[index] == currentBit)
+                {
+                    length++;
+                    index++;
+                }
 
                 var lengthInTimeUnits = length / timeUnit;
 
                 switch (currentBit)
                 {
                     case '1':
-                        morse += lengthInTimeUnits == 1 ? "." : "-";
+                        morse.Append(lengthInTimeUnits == 1 ? "." : "-");
                         break;
                     case '0':
-                        morse += lengthInTimeUnits == 3 ? " " : lengthInTimeUnits == 7 ? "   " : "";
+                        morse.Append(lengthInTimeUnits == 3 ? " " : lengthInTimeUnits == 7 ? "   " : "");
                         break;
                 }
-
-                if (index == bits.Length - 1) break;
             }
 
-            return morse;
+            return morse.ToString();
         }
 
         public static string DecodeMorse(string morseCode)
         {
-            return string.Empty;
+            var result = new StringBuilder();
+            var words = morseCode.Split(new string[] { "   " }, System.StringSplitOptions.None);
+
+            foreach (var word in words)
+            {
+                foreach (var cr in word.Split(' '))
+                {
+                    result.Append(morseDict[cr]);
+                }
+                result.Append(" ");
+            }
+
+            return result.ToString().ToUpper().TrimEnd(' ');
         }
     }
 }
--- katas/katas/MorseCode/MorseCodeDecoderTest.cs
+++ katas/katas/MorseCode/MorseCodeDecoderTest.cs
@@ -5,9 +5,33 @@ namespace katas.MorseCode
     public class MorseCodeDecoderTest
     {
         [Test]
-        public void TestExampleFromDescription()
+        public void TestExampleFromDescriptionTest()
         {
             Assert.AreEqual("HEY JUDE", MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")));
+        }
+        
+        [Test]
+        public void MultipleBitsPerDotTest()
+        {
+            Assert.AreEqual("HEY", MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits("111100001111000011110000111100000000000011110000000000001111111111110000111100001111111111110000111111111111")));
+        }
+
+        [Test]
+        public void SingleDotTest()
+        {
+            Assert.AreEqual("E", MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits("1")));
+        }
+
+        [Test]
+        public void SingleLongDotTest()
+        {
+            Assert.AreEqual("E", MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits("111")));
+        }
+
+        [Test]
+        public void EETest()
+        {
+            Assert.AreEqual("EE", MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits("10001")));
         }
     }
 }
