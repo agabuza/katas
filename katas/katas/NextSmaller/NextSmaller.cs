b22d4fc71a347affae760e9a84156b98be3632ec
Anatolii Gabuza <anatolii.gabuza>
2020-07-23 22:59:09 +0200

    next smallest kata

---
+++ katas/katas/NextSmaller/NextSmaller.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+
+/// <summary>
+/// https://www.codewars.com/kata/5659c6d896bc135c4c00021e
+/// </summary>
+namespace katas.NextSmaller
+{
+    public class NextSmallerKata
+    {
+        public static long NextSmaller(long n)
+        {
+            var digits = n.ToString().ToArray();
+
+            for (int i = digits.Length - 2; i >= 0; i--)
+            {
+                if (digits[i + 1] < digits[i])
+                {
+                    var position = 0;
+                    var min = 0;
+                    var shouldSwap = false;
+                    for (int j = i + 1; j < digits.Length; j++)
+                        if (digits[i] > digits[j])
+                        {
+                            shouldSwap = true;
+                            min = digits[j];
+                            position = j;
+                        }
+
+                    if (shouldSwap)
+                    {
+                        var minValue = digits[i];
+                        digits[i] = digits[position];
+                        digits[position] = minValue;
+                    }
+
+                    var firstPart = digits.Take(i + 1).ToArray();
+
+                    if (firstPart[0] == '0') return -1;
+
+                    var secondPart = digits.Skip(i + 1).Take(n.ToString().Length - i - 1).OrderByDescending(x => x).ToArray();
+
+                    return long.Parse(new string(firstPart) + new string(secondPart));
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ katas/katas/NextSmaller/NextSmallerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace katas.NextSmaller
+{
+    public class NextSmallerTests
+    {
+        [TestCase(21, ExpectedResult = 12)]
+        [TestCase(907, ExpectedResult = 790)]
+        [TestCase(531, ExpectedResult = 513)]
+        [TestCase(1027, ExpectedResult = -1)]
+        [TestCase(1207, ExpectedResult = 1072)]
+        [TestCase(441, ExpectedResult = 414)]
+        [TestCase(29009, ExpectedResult = 20990)]
+        [TestCase(123456798, ExpectedResult = 123456789)]
+        public long FixedTests(long n)
+        {
+            return NextSmallerKata.NextSmaller(n);
+        }
+    }
+}
