6e350942d6b52b48fb6d3ec5eaf5a199b3612864
agrocks <gabuzaav@gmail.com>
2020-10-24 00:06:38 +0200

    Max Subarray sum

---
+++ katas/katas/MaxSubarray/MaxSubArray.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+/// </summary>
+namespace katas.MaxSubarray
+{
+    public class MaxSubArray
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            var sum = 0;
+            var maxSum = 0;
+            
+            for (var i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i] < 0 ? 0 : sum + arr[i];
+                if (maxSum < sum) maxSum = sum;
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ katas/katas/ParseIntReload/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+/// <summary>
+/// https://www.codewars.com/kata/525c7c5ab6aecef16e0001a5/train/csharp
+/// </summary>
+namespace katas.ParseIntReload
+{
+    class Parser
+    {
+        internal static int ParseInt(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
