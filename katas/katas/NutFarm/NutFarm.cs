aa2561306b39cf045fef1714bdd2997ff8391b4e
agrocks <gabuzaav@gmail.com>
2020-10-27 19:49:27 +0100

    NutFarm

---
+++ katas/katas/NutFarm/NutFarm.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+/// <summary>
+/// https://www.codewars.com/kata/59884371d1d8d3d9270000a5
+/// </summary>
+namespace katas.NutFarm
+{
+    public class NutFarm
+    {
+        public static int[] ShakeTree(string[] tree)
+        {
+            var nuts = tree[0].Select(v => v == 'o' ? 1 : 0).ToArray();
+            for (int i = 1; i < tree.Length; i++)
+            {
+                var currentLevel = tree[i];
+                for (int j = 1; j < nuts.Length; j++)
+                {                    
+                    switch (currentLevel[j])
+                    {
+                        case '\\': 
+                            nuts[j + 1] += nuts[j]; 
+                            nuts[j] = 0; 
+                            break;
+                        case '/':
+                            nuts[j - 1] += nuts[j];
+                            nuts[j] = 0;
+                            break;
+                        case '_': 
+                            nuts[j] = 0; 
+                            break;                        
+                    }
+                }
+            }
+
+            return nuts;
+        }
+    }
+}
