76e1dd045f53b8c58225c1427608e3e44ec8150c
agrocks <gabuzaav@gmail.com>
2020-11-04 23:45:42 +0100

    Block Squences v1.0

---
+++ katas/katas/BlockSequence/BlockSequence.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace katas.BlockSequence
+{
+    class BlockSequence
+    {
+        public static int solve(long n)
+        {
+            if (n <= 2) return 1;
+            long targetPos = n;
+            long currentLength = 0;
+            long totalLength = 0;
+            long sqNum = 0;
+            long zeroCount = 10;
+            var newZero = 0;
+            while(totalLength < n)
+            {
+                targetPos -= currentLength;
+                sqNum++;
+
+                if (sqNum >= zeroCount)
+                {
+                    newZero++;
+                    zeroCount *= 10;
+                }
+
+                var numLength = (newZero + 1);
+                totalLength += currentLength + numLength;
+                currentLength += numLength;
+            }
+
+            var sq = new StringBuilder();
+            while(sq.Length <= currentLength && sqNum > 0)
+            {
+                sq.Insert(0, sqNum);
+                sqNum--;
+            }
+
+            var res = sq[(int)(targetPos - 1)] - '0';
+            return res;
+        }
+    }
+}
---
+++ katas/katas/BlockSequence/BlockSequenceTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace katas.BlockSequence
+{
+    class BlockSequenceTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(1, BlockSequence.solve(1));
+            Assert.AreEqual(1, BlockSequence.solve(2));
+            Assert.AreEqual(2, BlockSequence.solve(3));
+            Assert.AreEqual(1, BlockSequence.solve(4));
+            Assert.AreEqual(2, BlockSequence.solve(5));
+            Assert.AreEqual(4, BlockSequence.solve(10));
+            Assert.AreEqual(1, BlockSequence.solve(100));
+            Assert.AreEqual(2, BlockSequence.solve(2100));
+            Assert.AreEqual(2, BlockSequence.solve(3100));
+            Assert.AreEqual(1, BlockSequence.solve(55));
+            Assert.AreEqual(6, BlockSequence.solve(123456));
+            Assert.AreEqual(3, BlockSequence.solve(123456789));
+        }
+
+        [Test]
+        public void PerformanceTests()
+        {
+            Assert.AreEqual(1, BlockSequence.solve(1000000000000000000));
+            Assert.AreEqual(7, BlockSequence.solve(999999999999999993));
+        }
+    }
+}
