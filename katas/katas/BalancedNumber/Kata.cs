8c5244796abafb4beac29bf6ffa1f63896cb5d72
Anatolii Gabuza <gabuzaav@gmail.com>
2021-10-18 19:32:51 +0200

    BalancedNumber

---
+++ katas/katas/BalancedNumber/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace katas.BalancedNumber
+{
+    class Kata
+    {
+        public static string BalancedNumber(int number)
+        {
+            var strNumber = number.ToString();
+
+            var midShift = strNumber.Length % 2 == 0 ? 1 : 0;
+            var sumLeft = strNumber.Substring(0, strNumber.Length / 2 - midShift).Sum(x => (int)x);
+            var sumRight = strNumber.Substring(strNumber.Length / 2 + midShift + strNumber.Length % 2, strNumber.Length / 2 - midShift).Sum(x => (int)x);
+
+            return sumLeft == sumRight ? "Balanced" : "Not Balanced";
+        }
+    }
+}
