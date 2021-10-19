1614a2f82a14a696fbdfe3af6865f1edbd6782bf
Anatolii Gabuza <gabuzaav@gmail.com>
2021-10-19 17:44:58 +0200

    ValidParentheses

---
+++ katas/katas/ValidParentheses/Kata.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace katas.ValidParentheses
+{
+    internal class Kata
+    {
+        public static bool ValidParentheses(string input)
+        {
+            var currentNode = new Node();
+            var stack = new Stack<Node>();
+            var nodes = new List<Node>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    currentNode = new Node
+                    {
+                        leftPos = i
+                    };
+
+                    stack.Push(currentNode);
+                    nodes.Add(currentNode);
+                }
+                else if (input[i] == ')' && stack.Count > 0)
+                {
+                    currentNode.rightPos = i;
+                    currentNode = stack.Pop();
+                }
+                else return false;
+            }
+
+            return stack.Count == 0;
+        }
+
+        public class Node
+        {
+            public int leftPos { get; set; }
+            public int rightPos { get; set; }
+        }
+    }
+}
---
+++ katas/katas/ValidParentheses/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace katas.ValidParentheses
+{
+        using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Kata.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Kata.ValidParentheses(")(((("));
+        }
+    }
+}
