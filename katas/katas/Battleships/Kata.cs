70fbd85e5a5ec4c824bb2a1ad0858b84aa602d1c
Anatolii Gabuza <gabuzaav@gmail.com>
2021-10-21 21:13:21 +0200

    Battleshipts

---
+++ katas/katas/Battleships/Kata.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace katas.Battleships
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/58d06bfbc43d20767e000074
+    /// </summary>
+    internal class Kata
+    {
+        public static Dictionary<string, double> damagedOrSunk(int[,] board, int[,] attacks)
+        {
+            // mark all hits with 2 on the map 
+            // go through the map and seach for -1 or 1           
+            // add to queue and start bfs l, r, t, d 
+            // pop from queue
+            // run
+
+            var result = new List<(string, double)>();
+
+            var originalBoard = (int[,])board.Clone();
+            
+            for (int i = 0; i < attacks.GetLength(0); i++)
+            {
+                var x = attacks[i, 1] - 1;
+                var y = attacks[i, 0] - 1;
+                if (board[x, y] > 0) board[x, y] = 9;
+            }
+
+            for (int i = 0; i < board.GetLength(0); i++)
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] > 0)
+                    {
+                        var assessment = InvestigateShip(board, i, j, originalBoard);
+                        result.Add((assessment.Item1, assessment.Item2));
+                    }
+                }
+
+            var res = result.GroupBy(x => x.Item1).ToDictionary(x => x.Key, x => (double)x.Count());
+            
+            if (!res.ContainsKey("sunk"))
+                res.Add("sunk", 0);
+
+
+            if (!res.ContainsKey("damaged"))
+                res.Add("damaged", 0);
+
+            if (!res.ContainsKey("notTouched"))
+                res.Add("notTouched", 0);
+
+            res.Add("points", result.Sum(x => x.Item2));
+
+            return res;
+        }
+
+        public static (string, double) InvestigateShip(int[,] board, int x, int y, int[,] originalboard)
+        {
+            var stateChanged = false;
+            var originalState = board[x, y];
+            var originalShipId = originalboard[x, y];
+            var queue = new Queue<(int, int, int)>();
+            double points = 0;
+            queue.Enqueue((x, y, board[x, y]));
+
+            while (queue.Count > 0)
+            {
+                var cell = queue.Dequeue();
+                var cellX = cell.Item1;
+                var cellY = cell.Item2; 
+                if (cellX + 1 < board.GetLength(0) && originalboard[cellX + 1, cellY] == originalShipId) queue.Enqueue((cellX + 1, cellY, board[cellX + 1, cellY]));
+                if (cellY + 1 < board.GetLength(1) && originalboard[cellX, cellY + 1] == originalShipId) queue.Enqueue((cellX, cellY + 1, board[cellX, cellY + 1]));
+
+                if (cell.Item3 == 9) points += 0.5;
+
+                if (cell.Item3 != originalState)
+                {
+                    stateChanged = true;
+                }
+
+                board[cellX, cellY] = -1;
+            }
+
+            var state = originalState == 9 ? // entrypoint was damaged
+                      (stateChanged ? "damaged" : "sunk") :
+                      (stateChanged ? "damaged" : "notTouched");
+
+            if (state == "sunk") points = 1;
+            if (state == "notTouched") points = -1;
+
+            return (state, points);
+
+        }
+    }
+}
---
+++ katas/katas/Battleships/Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace katas.Battleships
+{
+    [TestFixture]
+    internal class Tests
+    {
+
+        [TestCase]
+        public void BasicTest1()
+        {
+            var kata = new Kata();
+            int[,] board = { { 0, 0, 1, 0 },
+                                 { 0, 0, 1, 0 },
+                                 { 0, 0, 1, 0 } };
+            int[,] attacks = { { 3, 1 }, { 3, 2 }, { 3, 3 } };
+            var result = Kata.damagedOrSunk(board, attacks);
+            Console.WriteLine("Test 1 - sunk = 1, damaged = 0, notTouched = 0, points = 1");
+            Assert.AreEqual(1, result["sunk"]);
+            Assert.AreEqual(0, result["damaged"]);
+            Assert.AreEqual(0, result["notTouched"]);
+            Assert.AreEqual(1, result["points"]);
+        }
+
+        [TestCase]
+        public void BasicTest2()
+        {
+            var kata = new Kata();
+            int[,] board = { { 3, 0, 1 },
+                                 { 3, 0, 1 },
+                                 { 0, 2, 1 },
+                                 { 0, 2, 0 } };
+            int[,] attacks = { { 2, 1 }, { 2, 2 }, { 3, 2 }, { 3, 3 } };
+            var result = Kata.damagedOrSunk(board, attacks);
+            Console.WriteLine("Test 2 - sunk = 1, damaged = 1, notTouched = 1, points = 0.5");
+            Assert.AreEqual(1, result["sunk"]);
+            Assert.AreEqual(1, result["damaged"]);
+            Assert.AreEqual(1, result["notTouched"]);
+            Assert.AreEqual(0.5, result["points"]);
+        }
+    }
+}
